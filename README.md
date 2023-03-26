269d10204c4f19405b0d40b8574eae95da499ed7
Ewa <hazla.ewa@gmail.com>
2023-03-26 21:31:11 +0100

    initial code for data cleaning

---
+++ README.md
@@ -0,0 +1,5 @@
+# Milestone 3
+Please note that Milestones 1 and 2 involved setting up the environment and getting familiar with the overall scope of the project. Milestone 3 involved: 
+
+1. Writing a series of simple functions in `tabular_data.py` file to load in the airbnb data from a csv file, carry out cleaning operations on the data and then save them to another csv.file. 
+2. Another function `load_airbnb` loads numeric columns of this dataset as pandas dataframe `features` and price per night column as `labels` in preparation for data analysis.
---
+++ tabular_data.py
@@ -0,0 +1,69 @@
+from ast import literal_eval
+import pandas as pd
+import numpy as np
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+
+
+rating_columns = [
+    'Cleanliness_rating',
+    'Accuracy_rating',
+    'Communication_rating',
+    'Location_rating',
+    'Check-in_rating',
+    'Value_rating']
+
+default_value_columns = [
+    'guests',
+    'beds',
+    'bathrooms',
+    'bedrooms']
+
+
+def remove_rows_with_missing_ratings(df, subset):
+    df.dropna(inplace=True, subset=subset)
+    return df
+
+
+def convert_string_list_to_string(input_string):
+    try:
+        output_list = pd.eval(input_string)
+        output_string = " ".join(output_list[1:])
+        return output_string
+    except:
+        return np.nan
+
+
+def combine_description_strings(df, subset):
+    df[subset] = df[subset].apply(convert_string_list_to_string)
+    df.dropna(inplace=True, subset=subset)
+    return df
+
+
+def set_default_feature_values(df, subset, default):
+    df[subset] = df[subset].replace(np.nan, 1)
+    return df
+
+
+def clean_tabular_data(df):
+    df = remove_rows_with_missing_ratings(df, rating_columns)
+    df = combine_description_strings(df, 'Description')
+    df = set_default_feature_values(df, default_value_columns, 1)
+    return df
+
+
+# add a provision to make sure this does not happen if file already exists
+# if __name__ == "__main__":
+#     df = pd.read_csv('listing.csv')
+#     df = clean_tabular_data(df)
+#     df.to_csv('clean_tabular_data.csv')
+
+
+def load_airbnb(df, subset, label):
+    features = df[subset]
+    labels = df[label]
+    return (features, labels)
+
+df = pd.read_csv('clean_tabular_data.csv')
+features, labels = load_airbnb(df, (rating_columns + default_value_columns), 'Price_Night') 
+print(features.head(), labels.head())
